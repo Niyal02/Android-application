1289ccd1a1f5ee7bc6eee90258cf9356ef7cdd02
Niyal02 <bhetwalniyal@gmail.com>
2021-07-23 22:00:09 +0545

    first_assignment

--- app/src/main/java/com/example/myapplication/table_layout.java
+++ app/src/main/java/com/example/myapplication/table_layout.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class framelayout extends AppCompatActivity {
+public class table_layout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_framelayout);
+        setContentView(R.layout.activity_table_layout);
     }
 }
